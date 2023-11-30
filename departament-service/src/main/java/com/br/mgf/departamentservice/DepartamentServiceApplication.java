3d0e37cef1ce4aad7fc2500a1b26988546acfd8f
Matheus Gomes Ferreira <re045787@qintess.com>
2023-11-30 19:25:37 -0300

    Create employee and departament services

---
+++ departament-service/src/main/java/com/br/mgf/departamentservice/DepartamentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.mgf.departamentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentServiceApplication.class, args);
+	}
+
+}
---
+++ departament-service/src/test/java/com/br/mgf/departamentservice/DepartamentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mgf.departamentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
