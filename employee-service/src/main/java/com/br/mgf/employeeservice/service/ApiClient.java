86ec2add040e55db97f3c1548a546079d8609121
matheus-gomesf <matheusg.code@gmail.com>
2023-12-19 21:06:59 -0300

    Fix services names and add load balance in open feing.

--- employee-service/src/main/java/com/br/mgf/employeeservice/service/ApiClient.java
+++ employee-service/src/main/java/com/br/mgf/employeeservice/service/ApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(contextId = "DEPARTMENT_SERVICE")
 public interface ApiClient {
 
     @GetMapping("departments/{code}")
