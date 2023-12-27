42f177b040107b30e6813568fe0b0ae81532265e
matheus-gomesf <matheusg.code@gmail.com>
2023-12-27 09:55:04 -0300

    Fix application name.

--- employee-service/src/main/java/com/br/mgf/employeeservice/service/ApiClient.java
+++ employee-service/src/main/java/com/br/mgf/employeeservice/service/ApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("DEPARTMENT_SERVICE")
+@FeignClient("DEPARTMENT-SERVICE")
 public interface ApiClient {
 
     @GetMapping("departments/{code}")
