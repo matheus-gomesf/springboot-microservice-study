44640212c9bbea8d10d117177bef8a417618265d
matheus-gomesf <matheusg.code@gmail.com>
2024-01-03 20:01:55 -0300

    Create config server project.

---
+++ config-server/src/test/java/com/br/mgf/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mgf.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
